77f56ef411168d31a83f2310be4cb03b39fa886c
pratapGitR <pchhetri027@gmail.com>
2024-01-20 02:28:51 -0600

    change 2

--- maven_pratap/src/main/java/maven_pratap/MaveGitTest.java
+++ maven_pratap/src/main/java/maven_pratap/MaveGitTest.java
@@ -6,7 +6,8 @@ public class MaveGitTest {
 	public static void main(String[] args) {
 	int a =2;
 	int b =5;
-	System.out.println("the sum is: "+ (a+b) );
+	int c = 2;
+	System.out.println("the sum is: "+ (a+b+c) );
 
 	}
 
