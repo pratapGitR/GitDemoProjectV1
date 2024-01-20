f9b30bd4f529c0d08b9191e2bbef14f0383b94a6
pratapGitR <pchhetri027@gmail.com>
2024-01-20 02:14:19 -0600

    first commit

---
+++ maven_pratap/src/main/java/maven_pratap/MaveGitTest.java
@@ -0,0 +1,13 @@
+package maven_pratap;
+
+
+public class MaveGitTest {
+
+	public static void main(String[] args) {
+	int a =2;
+	int b =5;
+	System.out.println("the sum is: "+ (a+b) );
+
+	}
+
+}
---
+++ maven_pratap/src/test/java/maven_pratap/mavenTest.java
@@ -0,0 +1,14 @@
+package maven_pratap;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class mavenTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
